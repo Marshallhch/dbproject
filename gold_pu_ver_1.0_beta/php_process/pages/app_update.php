e25b938c7e8ae2f8f205fb252f0aa5e833a77ac2
Marshallhch <Marshallhch@gmail.com>
2021-02-04 12:30:19 +0900

    02/04 db project 1차 베타 퍼블리싱

--- gold_pu_ver_1.0_beta/php_process/pages/app_update.php
+++ gold_pu_ver_1.0_beta/php_process/pages/app_update.php
@@ -22,14 +22,6 @@ $sub_name = $_FILES['app_sub']['name'];
 $sub_tmp_name = $_FILES['app_sub']['tmp_name'];
 $sub_error = $_FILES['app_sub']['error'];
 
-// echo $app_title,
-// $app_serial,
-// $app_client,
-// $app_desc,
-// $regist_day,
-// $main_name,
-// $sub_name;
-
 //main image upload
 if($main_name && !$main_error){
   $uploaded_main_file = $img_upload_dir.$main_name;
@@ -62,5 +54,36 @@ if($sub_name && !$sub_error){
 
 //database connect
 include $_SERVER['DOCUMENT_ROOT']."/gold/php_process/connect/db_connect.php";
+$sql="update gold_app set GOLD_APP_tit='$app_title', GOLD_APP_ser='$app_serial', 	GOLD_APP_des='$app_desc', GOLD_APP_img='$main_name', GOLD_APP_thumb='$sub_name', GOLD_APP_cli='$app_client', GOLD_APP_reg='$regist_day' where GOLD_APP_num=$app_update_num";
 
+mysqli_query($dbConn, $sql);
+
+$sql="select * from gold_app order by GOLD_APP_num desc";
+
+$app_result = mysqli_query($dbConn, $sql);
+  
+  $arr_result = array();
+
+  while($app_row = mysqli_fetch_array($app_result)){
+    array_push($arr_result, array(
+      'appnum' => $app_row['GOLD_APP_num'],
+      'apptitle' => $app_row['GOLD_APP_tit'],
+      'appser' => $app_row['GOLD_APP_ser'],
+      'appdes' => $app_row['GOLD_APP_des'],
+      'appmain' => $app_row['GOLD_APP_img'],
+      'appthumb' => $app_row['GOLD_APP_thumb'],
+      'appclient' => $app_row['GOLD_APP_cli'],
+      'appreg' => $app_row['GOLD_APP_reg']
+    ));
+  }
+
+  //make json file
+  file_put_contents($_SERVER['DOCUMENT_ROOT'].'/gold/data/json/app.json', json_encode($arr_result, JSON_PRETTY_PRINT));
+
+  echo "
+    <script>
+      alert('수정이 완료되었습니다.');
+      location.href='/gold/pages/app/app.php';
+    </script>
+  ";
 ?>
